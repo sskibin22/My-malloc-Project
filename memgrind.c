8a3a90152cd6de1e61fcd07c56bb3dc2f0dfcfcb
sskibin22 <50159952+sskibin22@users.noreply.github.com>
2022-02-10 17:35:47 -0500

    Initial setup for all files

---
+++ memgrind.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mymalloc.h"
+
+int main(int argc, char**argv)
+{
+    //TODO
+
+    return EXIT_SUCCESS;
+}
---
+++ mymalloc.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mymalloc.h"
+
+
+#define MEMSIZE 4096
+
+static char memory[MEMSIZE];
+
+void *mymalloc(size_t size, char *file, int line)
+{
+    //TODO
+    return NULL;
+}
+
+void myfree(void *p, char *file, int line)
+{
+    //TODO
+}
