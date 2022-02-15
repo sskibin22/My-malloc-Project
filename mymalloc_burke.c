f978869cb50918b00e82f7c55d9b027cd98b0224
Scott Burke <sgburke@scarletmail.rutgers.edu>
2022-02-14 19:49:17 -0500

    Wrote coalesce() and get_header()

--- mymalloc_burke.c
+++ mymalloc_burke.c
@@ -19,9 +19,42 @@ void myfree(void *p, char *file, int line)
 }
 
 int coalesce(void *p) {
-    // coalesce the subsequent and previous chunks with the chunk pointed to by *p
-    // return the number of chunks coalesced with the chunk pointed to by *p
-    return 1;
+    // coalesce the next and previous chunks with the current chunk (pointed to by *p)
+    // return the number of chunks coalesced with the current chunk
+    int num_coal = 0;
+
+    // try to coalesce the current and next chunks
+    header_t *p_header = get_header(p);
+    header_t *next_header = p_header->next;
+
+    if (next_header != NULL && !next_header->allocated) {
+        p_header->next = next_header->next;
+        p_header->size += next_header->size + sizeof(header_t);
+        num_coal++;
+    }
+
+    // try to coalesce the previous and current chunks
+    // start at beginning of memory, traverse linked list until we reach the previous chunk
+    header_t *h = (header_t *)memory;
+    while (h < p_header) {
+        if (h->next == p_header && !h->allocated) {
+            h->next = p_header->next;
+            h->size += p_header->size + sizeof(header_t);
+            num_coal++;
+            break;
+        }
+        else {
+            h = h->next;
+        }
+    }
+
+    return num_coal;
+}
+
+header_t *get_header(void *p) {
+    // given a pointer to a chunk of memory, return a pointer to that chunk's header
+    // TODO: decide whether to test that p points to an address assigned by mymalloc()
+    return (header_t *)p - 1;
 }
 
 int is_initialized() {
--- mymalloc_burke.h
+++ mymalloc_burke.h
@@ -12,4 +12,5 @@ struct header {
 void *mymalloc(size_t size, char *file, int line);
 void myfree(void *p, char *file, int line);
 int coalesce(void *p);
+header_t *get_header(void *p);
 int is_initialized();
