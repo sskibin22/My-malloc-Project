08937c956db5673d96607d22833a36845967a7eb
Scott Burke <sgburke@scarletmail.rutgers.edu>
2022-02-14 20:09:08 -0500

    Move functions to main source files

--- mymalloc.h
+++ mymalloc.h
@@ -1,5 +1,16 @@
 #define malloc(s) mymalloc(s, __FILE__, __LINE__)
 #define free(p) myfree(p, __FILE__, __LINE__)
 
+typedef struct header header_t;
+
+struct header {
+    int allocated;
+    int size;
+    header_t *next;
+};
+
 void *mymalloc(size_t size, char *file, int line);
 void myfree(void *p, char *file, int line);
+int coalesce(void *p);
+header_t *get_header(void *p);
+int is_initialized();
